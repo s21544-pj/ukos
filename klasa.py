b4756090b5945fa4db6e0789f163b1764fb51404
s21544 <s21544@pjwstk.edu.pl>
2019-12-14 13:46:34 +0100

    end

--- klasa.py
+++ klasa.py
@@ -1,10 +1,15 @@
 import sys
 
+class Inna:
+    def __str__(self):
+        return "siema"
 class Klasa:
     def __init__(self,x):
         self.x=x
     def string(self,s):
         return self.x.join(s)
 
+i=Inna()
+print(i)
 k = Klasa('x')
 print(k.string(sys.argv))
